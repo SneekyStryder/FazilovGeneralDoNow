c38fa6d130c374fb104b28fc228640da1038f32b
Imran Fazilov <ifazilov3584@bths.edu>
2022-11-28 11:38:31 -0500

    done

--- src/Date.java
+++ src/Date.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Date {
+public class Date {
+
+    private int year;
+    private int month;
+    private int day;
+
+    public Date() {
+        /** sets to a default of 1/1/1970 */
+        year = 1970;
+        month = 1;
+        day = 1;
+    }
+
+    //Assume the day starts and 1 month at 1 and year is four digits
+    public Date(int year, int month, int day) {
+        /** Initialize to the parameter dates */
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public void print() {
+        /** print in date it the form day/month/year */
+        System.out.println(day + "/" + month + "/" + year);
+    }
+
 }
--- src/DateRunner.java
+++ src/DateRunner.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class DateRunner {
+//Try it out in this runner
+public class DateRunner
+{
+    public static void main(String[] args)
+    {
+//1. Construct the object defaultDate and then use print() to see if it’s working
+        Date defaultDate = new Date();
+        defaultDate.print();
+//2. Construct the object myFavoriteDate with the date of your choice and then use print() to see if it’s working.
+        Date myFavoriteDate = new Date(1717, 17, 17);
+        myFavoriteDate.print();
+    }
 }
